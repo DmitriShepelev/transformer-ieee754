386f046975c9bf492367022c014070565f732402
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-05-21 14:47:08 +0000

    Update TransformerTests.cs

--- TransformerTask.Tests/TransformerTests.cs
+++ TransformerTask.Tests/TransformerTests.cs
@@ -1,3 +1,4 @@
+using System;
 using NUnit.Framework;
 
 namespace TransformerTask.Tests
@@ -5,23 +6,33 @@ namespace TransformerTask.Tests
     [TestFixture]
     public class TransformerTests
     {
-        [TestCase(new[] {-255.255, 255.255, 4294967295.0, double.Epsilon, double.NaN, double.MinValue},
+        [TestCase(
+            new[]
+            {
+                122.625, -255.255, 255.255, 4294967295.012, -451387.2345, 0.2345E-12
+            },
             ExpectedResult = new[]
             {
+                "0100000001011110101010000000000000000000000000000000000000000000",
                 "1100000001101111111010000010100011110101110000101000111101011100",
                 "0100000001101111111010000010100011110101110000101000111101011100",
-                "0100000111101111111111111111111111111111111000000000000000000000",
-                "0000000000000000000000000000000000000000000000000000000000000001",
-                "1111111111111000000000000000000000000000000000000000000000000000",
-                "1111111111101111111111111111111111111111111111111111111111111111"
+                "0100000111101111111111111111111111111111111000000110001001001110",
+                "1100000100011011100011001110110011110000001000001100010010011100",
+                "0011110101010000100000000110000001011111000011101110100001011011"
             })]
-        [TestCase(new[] {double.PositiveInfinity, 0.0, double.NegativeInfinity, -0.0},
+        [TestCase(
+            new[]
+            {
+                double.PositiveInfinity, 0.0, double.NegativeInfinity, -0.0, double.Epsilon, double.NaN
+            },
             ExpectedResult = new[]
             {
                 "0111111111110000000000000000000000000000000000000000000000000000",
                 "0000000000000000000000000000000000000000000000000000000000000000",
                 "1111111111110000000000000000000000000000000000000000000000000000",
-                "1000000000000000000000000000000000000000000000000000000000000000"
+                "1000000000000000000000000000000000000000000000000000000000000000",
+                "0000000000000000000000000000000000000000000000000000000000000001",
+                "1111111111111000000000000000000000000000000000000000000000000000"
             })]
         public string[] TransformTests(double[] source)
         {
@@ -29,6 +40,14 @@ namespace TransformerTask.Tests
             return transformer.Transform(source);
         }
 
-        //TODO: Add tests for Exception cases here.
+        [Test]
+        public void Transform_ArrayIsNull_ThrowArgumentNullException() =>
+            Assert.Throws<ArgumentNullException>(() => new Transformer().Transform(null),
+                "Array cannot be null.");
+
+        [Test]
+        public void FilterArrayByKey_ArrayLengthEqualToZero_ThrowArgumentException() =>
+            Assert.Throws<ArgumentException>(() => new Transformer().Transform(new double[] { }),
+                "Array cannot be empty.");
     }
 }
